d24acf3a73182f5749eb7c6ab816f875e6f185c5
voitelo <bery7yahoo.de@gmail.com>
2025-08-22 17:54:03 +0200

    Delete custom-arch-install directory

--- custom-arch-install/config.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Locale and Keyboard
-LOCALE="en_GB.UTF-8"
-KEYMAP="de"
-
-# Base packages (kernel not included by default)
-PACKAGES_BASE="base linux-firmware sudo efibootmgr networkmanager fastfetch"
